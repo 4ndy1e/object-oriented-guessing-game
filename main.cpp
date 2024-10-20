cd9e5e6335ceb33dcbd5707b9d1b0882b904de73
Andy Le <4ndyle@gmail.com>
2024-10-19 21:30:15 -0700

    define classes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
 int main(int argc, const char* argv[]) {
 
 }
+
+class Player {
+  public: 
+
+  private:
+    int name, guesses;
+};
+
+class Leaderboard {
+  public:
+    Leaderboard();
+    void InsertPlayer(Player player);
+
+  private:
+    static const int NUM_LEADERS = 5;
+    Player leader[NUM_LEADERS];
+};
